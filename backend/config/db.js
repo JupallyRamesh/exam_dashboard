47d4de2f1a9582adb0c3be021c07fd8afa19700e
JupallyRamesh <rameshjupally770@gmail.com>
2025-05-14 15:34:38 +0530

    Updated database connection code for Render

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'ramesh',
-    database: 'exam_system'
+    host: 'dpg-d0i2flruibrs739sp6k0-a',
+    user: 'ramesh',
+    password: 'jcZjGEGQ8i1y60yV3XfdUTjJTLag8B8n',
+    database: 'ramesh'
 });
 
 db.connect((err) => {
     if (err) throw err;
-    console.log('Connected to MySQL Database.');
+    console.log('✅ Connected to Render MySQL Database');
 });
 
 module.exports = db;
